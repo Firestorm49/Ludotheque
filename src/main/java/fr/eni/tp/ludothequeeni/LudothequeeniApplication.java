a5db29cba488f7d8b9f79f9528e99a5710ae25e0
Firestorm49 <landry.guillet3@gmail.com>
2025-04-15 16:55:30 +0200

    new file:   .gitattributes 	new file:   .gitignore 	new file:   build.gradle 	new file:   gradle/wrapper/gradle-wrapper.jar 	new file:   gradle/wrapper/gradle-wrapper.properties 	new file:   gradlew 	new file:   gradlew.bat 	new file:   settings.gradle 	new file:   src/main/java/fr/eni/tp/ludothequeeni/LudothequeeniApplication.java 	new file:   src/main/resources/application.properties 	new file:   src/test/java/fr/eni/tp/ludothequeeni/LudothequeeniApplicationTests.java

---
+++ src/main/java/fr/eni/tp/ludothequeeni/LudothequeeniApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.tp.ludothequeeni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LudothequeeniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LudothequeeniApplication.class, args);
+	}
+
+}
